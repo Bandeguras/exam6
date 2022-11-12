e1e1073ffcf14cccbe2fc2c2388607cb6143f025
Said Saliev <repsaid@gmail.com>
2022-11-12 13:37:20 +0600

    fix mistakes

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -8,6 +8,6 @@ class Book(models.Model):
     author = models.CharField(max_length=30, verbose_name="Название")
     email = models.EmailField(max_length=45, verbose_name="Почта")
     description = models.TextField(max_length=3000, verbose_name="Описание")
-    completion_at = models.DateField(auto_now_add=True, verbose_name="Дата выполнения")
-    updated_at = models.DateField(auto_now=True, verbose_name="Дата обновления")
+    completion_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата выполнения")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     status = models.CharField(max_length=30, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name="Статус")
