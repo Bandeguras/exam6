fc83246f9241e0738c985a172907c7092629464e
Said Saliev <repsaid@gmail.com>
2022-11-12 13:28:14 +0600

    add webapp in settings.py and create models

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+
+class Book(models.Model):
+    author = models.CharField(max_length=30, verbose_name="Название")
+    email = models.EmailField(max_length=45, verbose_name="Почта")
+    description = models.TextField(max_length=3000, verbose_name="Описание")
+    completion_at = models.DateField(auto_now_add=True, verbose_name="Дата выполнения")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата обновления")
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name="Статус")
