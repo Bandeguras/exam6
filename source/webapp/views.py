8b3bb6400f1b1f20adba2169c4275de0a7a63e4a
Said Saliev <repsaid@gmail.com>
2022-11-12 14:29:15 +0600

    create main page and base html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Book
+# from webapp.form import TaskForm
 # Create your views here.
+
+
+def index_view(request):
+    books = Book.objects.order_by('-completion_at')
+    context = {
+        'books': books,
+    }
+    return render(request, 'index', context)
